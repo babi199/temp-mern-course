58b39d61edc15273028acf0de43c4f5c7df48b76
chaitanyakund <kundachaitanya1999@gmail.com>
2024-08-24 13:04:10 +0530

    user image fix

--- contollers/userController.js
+++ contollers/userController.js
@@ -2,7 +2,8 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
 import Job from "../models/JobModel.js";
 import cloudinary from "cloudinary";
-import { promises as fs } from "fs";
+
+import { formatImage } from "../middleware/multerMiddleWare.js";
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
@@ -19,14 +20,12 @@ export const getApplicationStats = async (req, res) => {
 export const updateUser = async (req, res) => {
   const newUser = { ...req.body };
   delete newUser.password;
-  console.log(req.file);
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path);
+    const file = formatImage(req.file);
+    const response = await cloudinary.v2.uploader.upload(file);
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
-
   const updatedUser = await User.findByIdAndUpdate(req.user.userId, newUser);
 
   if (req.file && updatedUser.avatarPublicId) {
